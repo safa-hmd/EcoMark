81c0442ce45cef17a92c51f2f7b0cc0e56d082c4
Malak-Messaoui <Malak.Messaoui@esprit.tn>
2025-12-06 13:18:28 +0100

    optimiser modifier reponse

--- src/Form/ReponseType.php
+++ src/Form/ReponseType.php
@@ -9,13 +9,21 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ReponseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Contenu');
+              ->add('contenu', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => [
+                    'placeholder' => 'Votre message (10 à 200 caractères)',
+                    'class' => 'form-control'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
